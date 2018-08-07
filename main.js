c393358d19a599aec478a4a5ad110501a76d07d1
SilverWolf32 <43831298+SilverWolf32@users.noreply.github.com>
2018-08-06 21:07:26 -0400

    Implement title bar

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const {app, BrowserWindow} = require('electron')
 
 function openMainWindow() {
-	let window = new BrowserWindow({width: 800, height: 600, titleBarStyle: "hiddenInset"})
+	let window = new BrowserWindow({
+		width: 800,
+		height: 600,
+		titleBarStyle: "hiddenInset",
+		darkTheme: true
+	})
 	window.loadFile("app/index.html")
 }
 
