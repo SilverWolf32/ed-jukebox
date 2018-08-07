ed5996b0e7b3711cad5d4672cc468b19ce80b105
SilverWolf32 <43831298+SilverWolf32@users.noreply.github.com>
2018-08-06 20:31:16 -0400

    Add footer

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const {app, BrowserWindow} = require('electron')
 
 function openMainWindow() {
-	let window = new BrowserWindow({width: 800, height: 600, titleBarStyle: "hidden"})
+	let window = new BrowserWindow({width: 800, height: 600, titleBarStyle: "hiddenInset"})
 	window.loadFile("app/index.html")
 }
 
