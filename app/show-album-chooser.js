4bacdee0814fde598bd137ad7dc461d7514485fb
SilverWolf32 <43831298+SilverWolf32@users.noreply.github.com>
2018-08-09 12:05:25 -0400

    Make overlay larger, automatically adjust it to not hit close button

--- app/show-album-chooser.js
+++ app/show-album-chooser.js
@@ -11,7 +11,7 @@
 		
 		// show actual overlay content
 		let overlay = document.getElementById("album-chooser-overlay")
-		overlay.style.top = "12.5%" // move up
+		overlay.style.top = "0%" // move up
 	}
 	
 	let albumChooserCancelButton = document.getElementById("album-chooser-cancel")
@@ -21,6 +21,6 @@
 		overlayContainer.style.visibility = "hidden"
 		
 		let overlay = document.getElementById("album-chooser-overlay")
-		overlay.style.top = "100%" // move up
+		overlay.style.top = "100%" // move down
 	}
 })()
