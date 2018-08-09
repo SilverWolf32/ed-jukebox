b6571d35cbedd4baf58ea700549dee88e7b1e805
SilverWolf32 <43831298+SilverWolf32@users.noreply.github.com>
2018-08-09 08:35:41 -0400

    Add overlay content container, slides in on show

--- app/show-album-chooser.js
+++ app/show-album-chooser.js
@@ -1,19 +1,26 @@
 // define a function and call it at the same time
 // allows returning
 ;(function() {
-	debugger
+	// debugger
 	let albumChooserButton = document.getElementById("album-chooser-button")
 	albumChooserButton.onclick = function() {
-		debugger
-		let overlay = document.getElementById("album-chooser-overlay-container")
-		overlay.style.visibility = "visible"
-		overlay.style.opacity = 1.0
+		// debugger
+		let overlayContainer = document.getElementById("album-chooser-overlay-container")
+		overlayContainer.style.visibility = "visible"
+		overlayContainer.style.opacity = 1.0
+		
+		// show actual overlay content
+		let overlay = document.getElementById("album-chooser-overlay")
+		overlay.style.top = "12.5%" // move up
 	}
 	
 	let albumChooserCancelButton = document.getElementById("album-chooser-cancel")
 	albumChooserCancelButton.onclick = function() {
-		let overlay = document.getElementById("album-chooser-overlay-container")
-		overlay.style.opacity = 0.0
-		overlay.style.visibility = "hidden"
+		let overlayContainer = document.getElementById("album-chooser-overlay-container")
+		overlayContainer.style.opacity = 0.0
+		overlayContainer.style.visibility = "hidden"
+		
+		let overlay = document.getElementById("album-chooser-overlay")
+		overlay.style.top = "100%" // move up
 	}
 })()
