95b5c2deed7d8649a8cbeb7876d525b8ab000b1b
SilverWolf32 <43831298+SilverWolf32@users.noreply.github.com>
2018-08-12 12:04:29 -0400

    Add drag and drop (no saving yet, no sorting)

--- app/playlist-organization.js
+++ app/playlist-organization.js
@@ -39,8 +39,16 @@
 			}
 			col0.innerHTML = `${zeros}${i+1}` */
 			col0.innerHTML = i+1
-			
 			col1.innerHTML = track
+			
+			row.draggable = true
+			row.ondragstart = function(event) {
+				event.dataTransfer.setData("application/editc-itunes-track", JSON.stringify({
+					"url": "",
+					"title": track,
+					"trackNumber": i+1
+				}))
+			}
 			
 			row.appendChild(col0)
 			row.appendChild(col1)
@@ -78,8 +86,16 @@
 			}
 			col0.innerHTML = `${zeros}${i+1}` */
 			col0.innerHTML = i+1
-			
 			col1.innerHTML = track
+			
+			row.draggable = true
+			row.ondragstart = function(event) {
+				event.dataTransfer.setData("application/editc-itunes-track", JSON.stringify({
+					"url": "",
+					"title": track,
+					"trackNumber": i+1
+				}))
+			}
 			
 			row.appendChild(col0)
 			row.appendChild(col1)
@@ -89,3 +105,99 @@
 		availableTracksContainer.appendChild(newHTML)
 	})
 })()
+
+function objectOrParentOfClass(obj, classname) {
+	var node = obj
+	while (node != null) {
+		// console.log("\tlooking at " + obj.className)
+		if (node.classList != null && node.classList.contains(classname)) {
+			// console.log("found parent: " + node.className)
+			return node // we're done!
+		}
+		node = node.parentElement // will be null if no parent
+	}
+	// console.log("couldn't find parent for node: " + obj)
+	return null // couldn't find anything
+}
+
+// set up dragging
+// see https://developer.mozilla.org/en-US/docs/Web/Events/dragstart#JavaScript_Content
+
+var currentDraggedTrack
+var _lastDragEntered
+
+// set up dragleave event listener on all playlist views to reset their highlight
+let playlistViews = document.querySelectorAll(".playlist-view")
+for (var i = 0; i < playlistViews.length; i++) {
+	let playlistView = playlistViews[i]
+	playlistView.addEventListener("dragleave", function(event) {
+		console.log("Drag left <" + event.target.nodeName + "> (class: " + event.target.className + ")")
+		console.log("[drag last entered <" + _lastDragEntered.nodeName + "> class: " + _lastDragEntered.className + "]")
+		if (!playlistView.contains(_lastDragEntered) && event.target == playlistView || !playlistView.contains(event.target)) { // make sure it's not a child element
+			console.log("Dragged outside, unhighlighting.")
+			playlistView.style = ""
+		} else {
+			console.log("Dragged over child!")
+		}
+	}, false)
+}
+
+function unhighlightAllPlaylistViews() {
+	// unhighlight all playlist views
+	let playlistViews = document.querySelectorAll(".playlist-view")
+	for (var i = 0; i < playlistViews.length; i++) {
+		let playlistView = playlistViews[i]
+		playlistView.style = ""
+	}
+}
+
+// events for draggable items
+document.addEventListener("drag", function(event) {
+	
+}, false) // false: event capturing instead of bubbling (reverse responder chain)
+document.addEventListener("drag", function(event) {
+	currentDraggedTrack = event.target
+	event.target.style.opacity = 0.5 // make half transparent
+}, false)
+document.addEventListener("dragend", function(event) {
+	event.target.style.opacity = "" // reset opacity
+	unhighlightAllPlaylistViews()
+}, false)
+
+// events for drop targets
+document.addEventListener("dragover", function(event) {
+	// prevent default to allow drop
+	event.preventDefault()
+}, false)
+document.addEventListener("dragenter", function(event) {
+	// highlight drop target
+	_lastDragEntered = event.target
+	console.log("Drag entered <" + event.target.nodeName + "> (class: " + event.target.className + ")")
+	var playlistView = objectOrParentOfClass(event.target, "playlist-view")
+	if (playlistView != null) {
+		unhighlightAllPlaylistViews() // reset
+		playlistView.style.border = "1px dashed #FF6000"
+		playlistView.style.padding = playlistView.style.padding + 1
+	}
+}, false)
+/* document.addEventListener("dragleave", function(event) {
+	// unhighlight drop target
+	// var playlistView = objectOrParentOfClass(event.target, "playlist-view")
+	// if (playlistView != null) {
+	if (event.target.classList != null && event.target.classList.contains("playlist-view")) {
+		event.target.style.border = ""
+	}
+}, false) */
+document.addEventListener("drop", function(event) {
+	// prevent default action (open as link for some elements)
+	event.preventDefault()
+	// move dragged element to the new table
+	let dropTarget = objectOrParentOfClass(event.target, "playlist-view")
+	// if (event.target.classList != null && event.target.classList.contains("playlist-view")) {
+	if (dropTarget != null) {
+		currentDraggedTrack.parentNode.removeChild(currentDraggedTrack)
+		dropTarget.appendChild(currentDraggedTrack)
+	}
+	
+	unhighlightAllPlaylistViews()
+}, false)
