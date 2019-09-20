ce371411c7989acf57e0027187c27d79f457c62c
SilverWolf <argentwolf@protonmail.com>
2019-09-20 15:41:16 -0400

    Simplify less.zsh globbing
    It makes it far easier to understand and seems to do the same thing.

--- less.zsh
+++ less.zsh
@@ -5,6 +5,5 @@ setopt extendedglob
 for file in app/**/*.less; do
 	echo "Converting $file"
 	filename=${file##*/}
-	# echo $file ${(S)file%%/[^/]*}/.compiled/${filename%%.less}.css
-	node_modules/less/bin/lessc $file ${(S)file%%/[^/]*}/.compiled/${filename%%.less}.css
+	node_modules/less/bin/lessc $file ${file%/*}/.compiled/${filename%%.less}.css
 done
